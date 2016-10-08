09a5bfb6ae3702fac9d4fdda54cfc4f19e1edbcf
Aravinda Rao <maniacalace@gmail.com>
2016-10-08 10:58:02 +0530

    Create directory during vim dotfiles installation for persistent undo

--- setup/install_dotfiles.py
+++ setup/install_dotfiles.py
@@ -6,8 +6,15 @@ from settings import DOTFILES_ROOT_DIR
 from utils.file_utils import install_dotfiles
 
 
-def install_all_dotfiles():
+def install_neovim_dotfiles():
     install_dotfiles('neovim', 'dotfiles/nvim', '~/.config/nvim')
+
+    # Make directory for persistent undo
+    os.makedirs(os.path.join(DOTFILES_ROOT_DIR, 'dotfiles/nvim/undo'), exist_ok=True)
+
+
+def install_all_dotfiles():
+    install_neovim_dotfiles()
     install_dotfiles(
         'vim_plug', 'dotfiles/nvim/vim_plug/plug.vim', '~/.config/nvim/autoload/plug.vim'
     )
