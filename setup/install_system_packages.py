a88ae5f90a203ece19a064831cceca2e1cda6dc2
Aravinda Rao <maniacalace@gmail.com>
2016-10-10 02:27:28 +0530

    Make zsh default shell during installation

--- setup/install_system_packages.py
+++ setup/install_system_packages.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 from plumbum import FG
-from plumbum.cmd import pacman
+from plumbum.cmd import pacman, chsh
 
 from utils.messaging import echo
 
 
 packages_to_install = [
-    'base-devel', 'tmux', 'git',
+    'base-devel', 'zsh', 'tmux', 'git',
 
     # For building neovim
     'cmake', 'unzip'
@@ -16,12 +16,16 @@ packages_to_install = [
 pacman_install = pacman['-S', '--noconfirm']
 
 
+def make_zsh_default_shell():
+    chsh['-s', '/bin/zsh'] & FG
+
+
 def install_system_packages():
     echo('Installing system packages...')
-
     pacman_install[packages_to_install] & FG
-
     echo('System packages installed!')
+
+    make_zsh_default_shell()
 
 
 if __name__ == '__main__':
