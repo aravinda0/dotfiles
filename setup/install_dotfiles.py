74df98ee1d4a459e41982f2763a6fe583b653448
Aravinda Rao <maniacalace@gmail.com>
2016-10-13 00:32:58 +0530

    Add beginnings of zsh config

--- setup/install_dotfiles.py
+++ setup/install_dotfiles.py
@@ -13,11 +13,17 @@ def install_neovim_dotfiles():
     os.makedirs(os.path.join(DOTFILES_ROOT_DIR, 'dotfiles/nvim/undo'), exist_ok=True)
 
 
+def install_zsh_dotfiles():
+    install_dotfiles('zshenv', 'dotfiles/zsh/zshenv', '~/.zshenv')
+    install_dotfiles('zshrc', 'dotfiles/zsh/zshrc', '~/.zshrc')
+
+
 def install_all_dotfiles():
     install_neovim_dotfiles()
     install_dotfiles(
         'vim_plug', 'dotfiles/nvim/vim_plug/plug.vim', '~/.config/nvim/autoload/plug.vim'
     )
+    install_zsh_dotfiles()
 
 
 if __name__ == '__main__':
