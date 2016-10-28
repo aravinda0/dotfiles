9b665c7408a1e36d83725fe4fa8b4bf29cd1669e
Aravinda Rao <maniacalace@gmail.com>
2016-10-28 22:16:54 +0530

    Print out no-op warning for fzf config installation

--- setup/fzf/install_config.py
+++ setup/fzf/install_config.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+from utils.messaging import echo
+
 
 def install_config():
-    # No-op since config gets installed alongside tools. See notes in
-    # `./install_tools.py`.
+    echo(
+        'fzf config installation is a no-op, since the config gets installed during '
+        'installation of fzf. See notes in `setup/fzf/install_tools.py`'
+    )
     pass
 
 if __name__ == '__main__':
