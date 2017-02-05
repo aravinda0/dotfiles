0e0f00deba1376be8c2668026198b9764d20d978
Aravinda Rao <maniacalace@gmail.com>
2017-02-05 13:05:22 +0530

    Add setup for ag

---
+++ setup/discoverable/ag/install_tools.py
@@ -0,0 +1,12 @@
+from plumbum import FG
+from utils.system import pacman_install_f
+
+from utils.messaging import echo
+
+
+def install_tools():
+    echo('Installing ag...')
+
+    pacman_install_f[['the_silver_searcher']] & FG
+
+    echo('Ag installed!')
