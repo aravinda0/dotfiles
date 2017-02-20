c8431c9530522d4ca60ea2603245aaa20c046262
Aravinda Rao <maniacalace@gmail.com>
2017-02-20 23:25:59 +0530

    Add alias that takes you to the git repo's root

--- dotfiles/zsh/config/aliases.zsh
+++ dotfiles/zsh/config/aliases.zsh
@@ -16,3 +16,7 @@ alias vim='nvim'
 alias ta='tmux attach -t'
 alias tn='tmux new -s'
 alias tls='tmux ls'
+
+
+# `cd` to git repo root
+alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
