ee7078608d4869f9912300f8e6140516bc6ed2f7
Aravinda Rao <maniacalace@gmail.com>
2017-03-31 00:43:48 +0530

    Fix plumbum call to mkdir in setup helper

--- setup/utils/system/install.py
+++ setup/utils/system/install.py
@@ -11,7 +11,7 @@ def install_from_source(program_name, git_repo, make_options=None):
     make_options = make_options or {}
 
     build_dir = settings.DOTFILES_BUILD_DIR
-    mkdir['-p'](build_dir)
+    mkdir['-p', build_dir] & FG
 
     with local.cwd(build_dir):
         if lexists(join(build_dir, program_name)):
