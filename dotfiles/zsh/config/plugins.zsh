01fdd2ed36213affb678a375be1cc5bf61df5524
Aravinda Rao <maniacalace@gmail.com>
2018-12-26 01:58:49 +0530

    feat(zim): Switch from zplug to zim for zsh

--- dotfiles/zsh/config/plugins.zsh
+++ dotfiles/zsh/config/plugins.zsh
@@ -1,21 +1,16 @@
-# Refer: https://github.com/zplug/zplug/wiki
-
 
 # Initialize plugin system
-export ZPLUG_HOME=$DOTFILES_PATH/zsh/zplug
-source $ZPLUG_HOME/init.zsh
+export ZIM_HOME=$HOME/.zim
+[[ -s ${ZIM_HOME}/init.zsh ]] && source ${ZIM_HOME}/init.zsh
 
 
 # -------------------------------------------------------------------------------
 # FZF
 # -------------------------------------------------------------------------------
 
-# Fetch fzf binary from release repo and make available as command
-zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
-
-# Use the fzf repo's helper shell scripts for key bindings and completion settings
-zplug "junegunn/fzf", from:github, use:"shell/*.zsh"
-
+# Source fzf helpers from installation site
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
 
 # Use `ag` instead of default `find` command
 if ag_cmd="$(type -p ag)" || [[ -z "$ag_cmd" ]]; then
@@ -35,21 +30,11 @@ bindkey '^p' fzf-file-widget
 # Minimal - A simple, clean prompt theme with 'magic enter' capabilities
 # -------------------------------------------------------------------------------
 
-zplug 'subnixr/minimal'
+source ${DOTFILES_PATH}/zsh/external/minimal/minimal.zsh
 
 
 # -------------------------------------------------------------------------------
 # fzf-marks - Bookmark directories and use fzf to open them
 # -------------------------------------------------------------------------------
 
-# NOTE: There's a bug in the script for this that seems to look for `fzf-tmux` and
-# fails. Modify the `fzfcmd()` function in there manually to return just 'fzf'.
-zplug 'urbainvaes/fzf-marks'
-
-
-# -------------------------------------------------------------------------------
-# Finish zplug setup
-# -------------------------------------------------------------------------------
-
-# Finally, load plugins and make available for use
-zplug load
+source $DOTFILES_PATH/zsh/external/fzf-marks/fzf-marks.plugin.zsh
--- setup/discoverable/zsh/install_config.py
+++ setup/discoverable/zsh/install_config.py
@@ -3,16 +3,27 @@ from plumbum.cmd import chsh
 
 from utils.file_utils import install_dotfiles
 from utils.messaging import echo
+from utils.system.commands import pacman_install_f
 
 
 def install_zsh_dotfiles():
     # Add shell startup files to standard locations
     install_dotfiles('zsh/zshenv', '~/.zshenv', 'zshenv')
     install_dotfiles('zsh/zshrc', '~/.zshrc', 'zshrc')
+    install_dotfiles('zsh/zlogin', '~/.zlogin', 'zlogin')
 
 
 def make_zsh_default_shell():
     chsh['-s', '/bin/zsh'] & FG
+
+
+def install_zim():
+    install_dotfiles('zsh/external/zimfw', '~/.zim', 'zim')
+    install_dotfiles('zsh/config/zim/zimrc', '~/.zimrc', 'zimrc')
+
+
+def install_zsh_plugins():
+    pacman_install_f['fzf'] & FG
 
 
 def install_config():
@@ -20,5 +31,7 @@ def install_config():
 
     make_zsh_default_shell()
     install_zsh_dotfiles()
+    install_zim()
+    install_zsh_plugins()
 
     echo('Zsh config installed!\n')
