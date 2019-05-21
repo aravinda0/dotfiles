0d2065bf396f5afa4b5347d211b74258295a9999
Aravinda Rao <maniacalace@gmail.com>
2019-05-22 01:33:13 +0800

    config(xcape): Add workaround for 'super' key bindings on xfce

--- dotfiles/xcape/xcape.sh
+++ dotfiles/xcape/xcape.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
-# Make 'caps lock' behave like 'escape' when tapped, or like 'ctrl' when used
+# We use xcape to handle some keypress behaviours:
+
+# 1. Make 'caps lock' behave like 'escape' when tapped, or like 'ctrl' when used
 # with another key
-xcape -e 'Caps_Lock=Escape;Control_L=Escape;Control_R=Escape'
+
+# 2. On xfce, there's an issue where the 'super' key is tied by default to the whisker-menu
+# and it happens on keydown - so any super-mappings aren't triggered.
+# We remap the default whisker menu to `Alt-F1` and use xcape to trigger that when super
+# is pressed by itself.
+
+xcape -e 'Caps_Lock=Escape;Control_L=Escape;Control_R=Escape;Super_L=Alt_L|F1'
