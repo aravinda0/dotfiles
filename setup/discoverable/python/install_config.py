9fe47e7881fdd098ec9244a89219768481d604c2
Aravinda Rao <maniacalace@gmail.com>
2020-05-19 21:27:15 +0530

    config(python): Add global flake8 config

--- setup/discoverable/python/install_config.py
+++ setup/discoverable/python/install_config.py
@@ -4,6 +4,7 @@ from utils.messaging import echo
 
 def install_config():
     install_poetry_config()
+    install_flake8_config()
 
 
 def install_poetry_config():
@@ -12,3 +13,9 @@ def install_poetry_config():
         'python/config.toml', '~/.config/pypoetry/config.toml', 'pypoetry_config'
     )
     echo('Poetry config installed!')
+
+
+def install_flake8_config():
+    echo('Installing global flake8 config...')
+    install_dotfiles('python/flake8', '~/.flake8', 'global_flake8')
+    echo('Global flake8 config installed!')
