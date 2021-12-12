0aca5ea54edb4702aadfd580852e114777a55631
Aravinda Rao <maniacalace@gmail.com>
2021-12-12 20:39:22 +0530

    config(zsh): Add git helper aliases

--- dotfiles/zsh/config/aliases.zsh
+++ dotfiles/zsh/config/aliases.zsh
@@ -11,6 +11,7 @@ alias o='gio open'
 
 
 alias vim='nvim'
+alias ag='rg'
 
 
 alias ta='tmux attach -t'
@@ -18,8 +19,16 @@ alias tn='tmux new -s'
 alias tls='tmux ls'
 
 
+# git helpers
+
+alias gs='git status'
+alias gd="git diff"
+alias gds="git diff --staged"
+alias ga='git add'
+alias gco='git commit'
+alias gci='git commit -m'
+alias gp='git push'
+
 # `cd` to git repo root
 alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
 
-
-alias ag='rg'
