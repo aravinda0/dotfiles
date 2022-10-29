dacb1ea8101909153104b3d15d3e48a08e74e98a
Aravinda Rao <maniacalace@gmail.com>
2022-10-29 16:07:24 +0530

    setup(nvim): Cleanup unused installation of formatters into neovim venv

--- setup/discoverable/nvim/install_config.py
+++ setup/discoverable/nvim/install_config.py
@@ -6,38 +6,25 @@ from plumbum import FG
 import settings
 from utils.file_utils import install_dotfiles
 from utils.messaging import echo
-from utils.system.commands import nvim_venv_pip
 
 
 def install_neovim_dotfiles():
-    echo('Installing neovim config...')
+    echo("Installing neovim config...")
 
-    install_dotfiles('nvim', '~/.config/nvim', 'neovim')
+    install_dotfiles("nvim", "~/.config/nvim", "neovim")
 
     # Make directory for persistent undo feature
-    os.makedirs(join(settings.DOTFILES_REPO_DOTFILES_DIR, 'nvim/.undo'), exist_ok=True)
+    os.makedirs(join(settings.DOTFILES_REPO_DOTFILES_DIR, "nvim/.undo"), exist_ok=True)
 
     # Install config for vim plugin manager
     install_dotfiles(
-        'nvim/vim_plug/plug.vim', '~/.config/nvim/autoload/plug.vim', 'vim_plug',
+        "nvim/vim_plug/plug.vim",
+        "~/.config/nvim/autoload/plug.vim",
+        "vim_plug",
     )
 
-    echo('Neovim config installed!\n')
-
-
-def install_plugin_python_dependencies():
-    echo('Installing python dependencies for neovim plugins...')
-
-    py_packages = [
-        'flake8',
-        'python-language-server[all]',
-    ]
-
-    nvim_venv_pip['install', py_packages] & FG
-
-    echo('Neovim plugin python dependencies installed!')
+    echo("Neovim config installed!\n")
 
 
 def install_config():
     install_neovim_dotfiles()
-    install_plugin_python_dependencies()
