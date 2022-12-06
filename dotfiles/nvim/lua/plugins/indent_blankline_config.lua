0b013468b4ffb646e6a67234e0ce105f9220ef5c
Aravinda Rao <maniacalace@gmail.com>
2022-12-06 18:40:33 +0530

    config(nvim): Have indent_blankline use treesitter

--- dotfiles/nvim/lua/plugins/indent_blankline_config.lua
+++ dotfiles/nvim/lua/plugins/indent_blankline_config.lua
@@ -1,11 +1,10 @@
 local M = {}
 
-
 M.setup = function()
   require("indent_blankline").setup({
-    filetype_exclude = { "markdown", "vimwiki" }
+    use_treesitter = true,
+    filetype_exclude = { "markdown", "vimwiki" },
   })
 end
-
 
 return M
