b2932af0ac2e22595214a086572a746e2361d7b9
Aravinda Rao <maniacalace@gmail.com>
2023-01-10 11:52:20 +0530

    config(qute): Some more config, bindings

--- dotfiles/qutebrowser/config.py
+++ dotfiles/qutebrowser/config.py
@@ -8,7 +8,15 @@
 
 - Generate file with default config:
     - `:config-write-py --defaults --force`
+
+- `:bind`
+    - to view all current bindings
+- `:set`
+    - to view all current options
+- `:process`
+    - to view userscript run info
 """
+
 
 # These are globals that qutebrowser makes available when processing this file. These
 # self-assignments are just to silence checkers and linters.
@@ -42,6 +50,9 @@ c.content.headers.user_agent = (
 )
 
 
+c.completion.open_categories = ["quickmarks", "bookmarks", "history", "filesystem"]
+
+
 c.auto_save.session = True
 
 
@@ -73,26 +84,39 @@ c.url.searchengines = {
 # Keymaps: normal mode
 # --------------------------------------------------------------------------------
 
+config.bind("<ctrl-p>", "set-cmd-text -s :open")
+config.bind("<ctrl-shift-p>", "spawn qutebrowser --target private-window")
+
+config.bind("<ctrl-t>", "open -t")
+config.bind("<ctrl-o>", "set-cmd-text -s :quickmark-load")
+
 config.bind("K", "tab-next")
 config.bind("J", "tab-prev")
+config.bind("<alt-r>", "tab-next")
+config.bind("<alt-e>", "tab-prev")
+config.bind("<ctrl-tab>", "tab-next")
+config.bind("<ctrl-shift-tab>", "tab-prev")
 
 config.bind("<ctrl-y>", "run-with-count 3 scroll up")
 config.bind("<ctrl-e>", "run-with-count 3 scroll down")
 
-config.bind("<ctrl-p>", "set-cmd-text -s :open")
-
-config.bind("<ctrl-shift-p>", "spawn qutebrowser --target private-window")
-
 # Hard refresh
 config.bind("<ctrl-shift-r>", "reload -f")
+
+
+config.bind("<ctrl-h>;m", "open -t;; bind")
 
 
 # --------------------------------------------------------------------------------
 # Keymaps: command mode
 # --------------------------------------------------------------------------------
 
+config.bind("<ctrl-e>", "completion-item-focus next;; rl-end-of-line", mode="command")
+config.bind("<ctrl-y>", "completion-item-focus prev", mode="command")
 config.bind("<ctrl-j>", "completion-item-focus next", mode="command")
 config.bind("<ctrl-k>", "completion-item-focus prev", mode="command")
+
+config.bind("<ctrl-return>", "spawn --userscript complete_tld.py", mode="command")
 
 
 # --------------------------------------------------------------------------------
