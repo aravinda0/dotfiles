ec1e900e8f5a8128eff6d1659f3f8037f374a1e0
Aravinda Rao <maniacalace@gmail.com>
2023-01-12 20:20:05 +0530

    config(lf): Add lf config

--- setup/discoverable/lf/install.py
+++ setup/discoverable/lf/install.py
@@ -1,5 +1,10 @@
+from utils.files import install_dotfiles
 from utils.system import system_install
 
 
 def install_tools():
     system_install("lf")
+
+
+def install_config():
+    install_dotfiles("lf", "~/.config/lf", "lf")
