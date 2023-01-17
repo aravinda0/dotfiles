85507e4a312c007673b8f564cb1e6af8176bb0cb
Aravinda Rao <maniacalace@gmail.com>
2023-01-17 12:53:45 +0530

    config(nvim): gruvbox in notes, melange in code

--- dotfiles/nvim/after/ftplugin/markdown.lua
+++ dotfiles/nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,5 @@ local keymaps = require("keymaps")
 keymaps.after_markdown()
 
 vim.o.textwidth = 80
+
+vim.cmd([[colorscheme gruvbox-material]])
--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
   {
+    "savq/melange-nvim",
+
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      vim.cmd([[colorscheme melange]])
+    end,
+  },
+  {
     "sainnhe/gruvbox-material",
-    enabled = false,
 
     lazy = false,
     priority = 1000,
@@ -10,18 +19,6 @@ return {
       vim.o.background = "dark"
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_better_performance = 1
-
-      vim.cmd([[colorscheme melange]])
-    end,
-  },
-  {
-    "savq/melange-nvim",
-
-    lazy = false,
-    priority = 1000,
-
-    config = function()
-      vim.cmd([[colorscheme melange]])
     end,
   },
 }
