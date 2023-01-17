b25d131737d47c1b80eec4bac03c533809f37a1b
Aravinda Rao <maniacalace@gmail.com>
2023-01-17 12:28:12 +0530

    config(nvim): Add mini-indenscope plugin

--- dotfiles/nvim/lua/plugins/indentation.lua
+++ dotfiles/nvim/lua/plugins/indentation.lua
@@ -1,6 +1,7 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
     event = "BufReadPre",
     opts = {
       filetype_exclude = {
@@ -17,5 +18,18 @@ return {
   {
     "Vimjas/vim-python-pep8-indent",
     ft = { "python" },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    event = "VeryLazy",
+    config = function()
+      local indentscope = require("mini.indentscope")
+      indentscope.setup({
+        draw = {
+          delay = 0,
+          animation = indentscope.gen_animation.none(),
+        },
+      })
+    end,
   },
 }
