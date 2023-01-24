d5faeacf8f399a2a8c67950bf042b3c050e20f99
Aravinda Rao <maniacalace@gmail.com>
2023-01-24 14:03:49 +0530

    config(X): Add config for default applications

--- setup/discoverable/X/install.py
+++ setup/discoverable/X/install.py
@@ -3,3 +3,5 @@ from utils.files import install_dotfiles
 
 def install_config():
     install_dotfiles("X/xprofile", "~/.xprofile", "xprofile")
+    install_dotfiles("X/desktop", "~/.local/share/applications", "desktop applications")
+    install_dotfiles("X/mimeapps.list", "~/.config/mimeapps.list", "mimeapps.list")
