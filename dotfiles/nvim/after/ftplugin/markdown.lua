5e46e4b42a8f3267762173fa228b21ac43ab11cc
Aravinda Rao <maniacalace@gmail.com>
2023-01-25 17:24:37 +0530

    config(nvim): diddle about with some colorscheme options

--- dotfiles/nvim/after/ftplugin/markdown.lua
+++ dotfiles/nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,5 @@ local keymaps = require("keymaps")
 keymaps.after_markdown()
 
 vim.o.textwidth = 80
+vim.g.gruvbox_material_background = "hard"
+vim.cmd([[colorscheme gruvbox-material]])
--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,43 @@
 return {
   {
     "savq/melange-nvim",
-
+    enabled = false,
     lazy = false,
     priority = 1000,
-
     config = function()
       vim.cmd([[colorscheme melange]])
     end,
   },
   {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme kanagawa]])
+    end,
+  },
+  {
+    "sainnhe/everforest",
+    -- enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_better_performance = 1
+      vim.cmd([[colorscheme everforest]])
+    end,
+  },
+  {
     "sainnhe/gruvbox-material",
+    -- enabled = false,
+    lazy = false,
+    priority = 1000,
     ft = { "markdown" },
 
     config = function()
       vim.o.background = "dark"
-      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_background = "medium"
       vim.g.gruvbox_material_better_performance = 1
       vim.cmd([[colorscheme gruvbox-material]])
     end,
