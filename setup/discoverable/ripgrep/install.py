9bca97decc4834628a5c015666297ffdf63710a2
Aravinda Rao <maniacalace@gmail.com>
2023-01-29 13:21:36 +0530

    config(ripgrep, zsh): incorporate rgignore and use symlink approach for ripgrep

--- setup/discoverable/ripgrep/install.py
+++ setup/discoverable/ripgrep/install.py
@@ -1,5 +1,11 @@
+from utils.files import install_dotfiles
 from utils.system import system_install
 
 
 def install_tools():
     system_install("ripgrep")
+
+
+def install_config():
+    install_dotfiles("ripgrep/ripgreprc", "~/.ripgreprc", "ripgreprc")
+    install_dotfiles("ripgrep/rgignore", "~/.rgignore", "rgignore")
