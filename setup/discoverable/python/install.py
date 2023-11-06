26d4a8cf057e5cb237b9ab9e6a24740ea1011337
Aravinda Rao <maniacalace@gmail.com>
2023-11-06 17:01:52 +0530

    setup(python): clean up python setup - have pipx at system level

--- setup/discoverable/python/install.py
+++ setup/discoverable/python/install.py
@@ -1,34 +1,25 @@
 import subprocess
 
 from utils.files import install_dotfiles
-from utils.system import pip_install
+from utils.system import system_install
 
 binary_packages = [
-    "poetry",
     "httpie",
     "ipython",
     "ptpython",
-    "black",
-    "isort",
+    "pdm",
 ]
 
 
 def install_tools():
     print("First installing pipx...")
-
-    pip_install("pipx")
-
+    system_install("python-pipx")
     print("pipx installed!")
 
     print("Installing python binary packages via pipx...")
-
     subprocess.run(["pipx", "install", *binary_packages])
-
     print("Python binary packages installed")
 
 
 def install_config():
-    install_dotfiles(
-        "python/config.toml", "~/.config/pypoetry/config.toml", "pypoetry_config"
-    )
     install_dotfiles("ptpython/config.py", "~/.config/ptpython/config.py", "ptpython")
