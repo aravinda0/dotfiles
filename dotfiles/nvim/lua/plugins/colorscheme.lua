98fb2aac3ed6432cb3e7aeb7d708ea261bd2ffd7
Aravinda Rao <maniacalace@gmail.com>
2023-11-06 18:02:42 +0530

    config(nvim): fix issue where triggering hover docs would trigger colorscheme switch

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -33,7 +33,7 @@ return {
     -- enabled = false,
     -- lazy = false,
     priority = 1001,
-    ft = { "markdown", "vimwiki" },
+    ft = { "vimwiki" },
     config = function()
       vim.o.background = "dark"
       vim.g.gruvbox_material_background = "medium"
