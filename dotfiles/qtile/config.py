88c76120a5958097bcdbd08b63d19d8fb26ba0f2
Aravinda Rao <maniacalace@gmail.com>
2023-12-23 23:10:23 +0530

    config(qtile): tweak float rules

--- dotfiles/qtile/config.py
+++ dotfiles/qtile/config.py
@@ -418,13 +418,10 @@ floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(wm_class="qbittorrent"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
+        Match(wm_class="ssh-askpass"),
+        Match(wm_class="qbittorrent"),
         Match(title="pinentry"),  # GPG key password entry
+        Match(wm_class="Pinentry-gtk-2"),
         Match(title="VeraCrypt"),
     ]
 )
