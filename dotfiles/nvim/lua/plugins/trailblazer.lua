b225804f4f1f243c228b0cac09ccd2eb9aa6b8f2
Aravinda Rao <maniacalace@gmail.com>
2024-04-08 17:05:05 +0530

    config(nvim): add some more default trailblazer stacks

--- dotfiles/nvim/lua/plugins/trailblazer.lua
+++ dotfiles/nvim/lua/plugins/trailblazer.lua
@@ -13,6 +13,11 @@ return {
           cursor_mark_symbol = "",
           next_mark_symbol = "",
           previous_mark_symbol = "",
+          default_trail_mark_stacks = {
+              "stack_0",
+              "stack_1",
+              "stack_2",
+          },
         },
         force_mappings = keymaps.build_trailblazer_keymaps(),
       })
