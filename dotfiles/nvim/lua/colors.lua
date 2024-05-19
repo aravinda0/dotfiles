6ab15c8e20bcfff6c006e39377d920edbde15c84
Aravinda Rao <maniacalace@gmail.com>
2024-05-19 15:48:49 +0530

    config(nvim): change from deprecated 'sign define' to 'vim.diagnostic.config(...)'

--- dotfiles/nvim/lua/colors.lua
+++ dotfiles/nvim/lua/colors.lua
@@ -6,9 +6,35 @@ vim.cmd([[
   highlight DiagnosticLineNrWarn guibg=#51412A guifg=#FFA500 gui=bold
   highlight DiagnosticLineNrInfo guibg=#1E535D guifg=#00FFFF gui=bold
   highlight DiagnosticLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
-
-  sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
-  sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
-  sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
-  sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
 ]])
+
+
+-- Keep sign column hidden - just underline issues.
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+    },
+    texthl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticLineNrError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticLineNrWarn',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticLineNrInfo',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticLineNrHint',
+    }
+  }
+})
