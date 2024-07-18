8fb2b8d66ab816399579393ea3074adbdd105df5
Aravinda Rao <maniacalace@gmail.com>
2024-07-18 09:41:13 +0530

    setup(rust): add cargo config

--- setup/discoverable/rust/install.py
+++ setup/discoverable/rust/install.py
@@ -1,5 +1,6 @@
 import subprocess
 
+from utils.files import install_dotfiles
 from utils.system import system_install
 
 
@@ -7,3 +8,7 @@ def install_tools():
     system_install("rustup")
     subprocess.run(["rustup", "default", "stable"])
     system_install("rust-analyzer")
+
+
+def install_config():
+    install_dotfiles("cargo/config.toml", "~/.cargo/config.toml", "cargo")
