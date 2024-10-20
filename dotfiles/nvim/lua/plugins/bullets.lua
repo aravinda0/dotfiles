6eb7b470ef1eb61795a796c48ea2956c8878471c
Aravinda Rao <maniacalace@gmail.com>
2024-10-20 13:42:16 +0530

    config(nvim): add bullets.vim

---
+++ dotfiles/nvim/lua/plugins/bullets.lua
@@ -0,0 +1,10 @@
+local keymaps = require("keymaps")
+
+return {
+  enabled = true,
+  "bullets-vim/bullets.vim",
+  ft = { "markdown", "text" },
+  init = function()
+    keymaps.set_bullets_keymaps()
+  end,
+}
