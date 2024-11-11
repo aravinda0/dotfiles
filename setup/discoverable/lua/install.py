0c1dbd544c17580613de70ac04e28b5508793083
Aravinda Rao <maniacalace@gmail.com>
2024-11-11 18:25:46 +0530

    setup(lua): add lua toolchain

---
+++ setup/discoverable/lua/install.py
@@ -0,0 +1,15 @@
+from utils.files import install_dotfiles
+from utils.system import system_install
+
+
+def install_tools():
+    system_install(["lua", "luajit", "luarocks", "lua-language-server"])
+
+
+def install_config():
+    # TODO: See if can get general config solution. Some platform dependency issues with
+    # luarocks? Config wants to be written into `~/.luarocks/config-5.4.lua`. Don't like
+    # the version specifier.
+
+    # install_dotfiles("lua/luarocks", "~/.luarocks", "luarocks")
+    pass
