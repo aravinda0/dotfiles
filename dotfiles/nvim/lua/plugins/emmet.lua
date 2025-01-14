a6824720acc575971a72c3253285435768bfba44
Aravinda Rao <maniacalace@gmail.com>
2025-01-14 23:41:32 +0530

    config(nvim): ensure some astro syntax stuff

--- dotfiles/nvim/lua/plugins/emmet.lua
+++ dotfiles/nvim/lua/plugins/emmet.lua
@@ -3,7 +3,7 @@ local keymaps = require("keymaps")
 return {
    {
       "mattn/emmet-vim",
-      ft = { "html", "css", "sass", "typescriptreact", "javascriptreact", "svelte" },
+      ft = { "html", "css", "sass", "typescriptreact", "javascriptreact", "svelte", "astro" },
       config = function()
          keymaps.set_emmet_keymaps()
       end,
--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
       config = function()
          require("nvim-treesitter.configs").setup({
             ensure_installed = {
+               "astro",
                "bash",
                "c",
                "css",
