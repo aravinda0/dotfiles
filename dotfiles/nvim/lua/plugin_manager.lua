7435fed96b58f2a403a309c95bf75b0a133dc7d6
Aravinda Rao <maniacalace@gmail.com>
2025-03-30 10:56:19 +0530

    nvim: update plugin manager bootstrap code

--- dotfiles/nvim/lua/plugin_manager.lua
+++ dotfiles/nvim/lua/plugin_manager.lua
@@ -1,17 +1,25 @@
 local bootstrap_lazy_nvim = function()
-   local lazy_nvim_path = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
-   if not vim.loop.fs_stat(lazy_nvim_path) then
-      vim.fn.system({
-         "git",
-         "clone",
-         "--filter=blob:none",
-         "https://github.com/folke/lazy.nvim.git",
-         "--branch=stable",
-         lazy_nvim_path,
-      })
+   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+   if not (vim.uv or vim.loop).fs_stat(lazypath) then
+      local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+      local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+      if vim.v.shell_error ~= 0 then
+         vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+         }, true, {})
+         vim.fn.getchar()
+         os.exit(1)
+      end
    end
-   vim.opt.rtp:prepend(lazy_nvim_path)
+   vim.opt.rtp:prepend(lazypath)
 end
 
 bootstrap_lazy_nvim()
-require("lazy").setup("plugins", opts)
+require("lazy").setup({
+   spec = {
+      -- import plugins dir with all its plugin specs
+      { import = "plugins" }
+   }
+})
